2be6553a10e8b8b0a08ae69ea16d8c894d3c717d
ALBERTO CRESPO SANCHEZ <67391243@iecisa.corp>
2018-08-04 14:24:20 +0200

    Api Object Scenes y Celebrities

--- proyectoFinalSls/handlers/handler.py
+++ proyectoFinalSls/handlers/handler.py
@@ -5,23 +5,35 @@ from handlers import utils_dynamodb
 from handlers import utils_rekognition
 
 def image_uploaded(event, context):
-    print('Event: ', event)
+
     file_obj = event['Records'][0]
     filePath = str(file_obj['s3']['object']['key'])
-    fileName = filePath.split('celebrities/')[1]
+    fileName = filePath.split('input_images/')[1]
     bucketName = file_obj['s3']['bucket']['name']
     
-    print('bucketName', bucketName)
-    print('filePath', filePath)
+
+    #Llamamos a la api que reconoce objetos y escenas
+    response = utils_rekognition.recognize_object_and_scenes(bucketName, filePath)
+
+    #Vemos si hay resultados
+    labels_object = utils_rekognition.parsear_resultado_object_and_scenes(response, 'Labels')
+
+    #Comprobamos si hay labels y si existen personas en ellas
+    size = len(labels_object) != 0
+    personas = utils_rekognition.comprobar_si_hay_persona(labels_object)
+    labels_celebrities = {}
+    if(size and personas):
+        #LLamamos a la api de celebridades
+        response = utils_rekognition.recognize_celebrities(bucketName, filePath)
+        labels_celebrities = utils_rekognition.parsear_resultado_object_and_scenes(response, 'CelebrityFaces')
     
-    response = utils_rekognition.recognize_celebrities(bucketName, filePath)
-    print('response', response)
+    print(labels_object)
+    print(labels_celebrities)
+    
+    #Guardamos los resultados en dynamo
+    #metodo para hacerlo
+    
 
-    if response["CelebrityFaces"]:
-        image_uuid = str(uuid.uuid1())
-
-        for celebrity in response["CelebrityFaces"]:
-            utils_dynamodb.create(image_uuid, celebrity)
 
 def dynamodb_celebrities(event, context):
     celebrities = utils_dynamodb.getCelebrities()
--- proyectoFinalSls/handlers/utils_rekognition.py
+++ proyectoFinalSls/handlers/utils_rekognition.py
@@ -3,6 +3,8 @@ import boto3
 
 rekognition = boto3.client('rekognition', region_name='eu-west-1')
 
+
+#Funcion para llamar reconocer celebridades
 def recognize_celebrities(bucketName, filePath):
     response = rekognition.recognize_celebrities(
         Image={
@@ -11,42 +13,44 @@ def recognize_celebrities(bucketName, filePath):
                 'Name': filePath
             }
         }
-        #"MinConfidence": 50
     )
 
     return response
 
-#EJEMPLO RESPONSE DE LA API REKOGNITION
-# {'CelebrityFaces': [{'Face': {'BoundingBox': {'Height': 0.45614033937454224,
-#      'Left': 0.25925925374031067,
-#      'Top': 0.06578947603702545,
-#      'Width': 0.2567901313304901},
-#     'Confidence': 99.99740600585938,
-#     'Landmarks': [{'Type': 'eyeLeft',
-#       'X': 0.3468391001224518,
-#       'Y': 0.26327452063560486},
-#      {'Type': 'eyeRight', 'X': 0.430842787027359, 'Y': 0.255566269159317},
-#      {'Type': 'nose', 'X': 0.3944968283176422, 'Y': 0.3450881540775299},
-#      {'Type': 'mouthLeft', 'X': 0.3547839820384979, 'Y': 0.4061143398284912},
-#      {'Type': 'mouthRight',
-#       'X': 0.4283067584037781,
-#       'Y': 0.40353700518608093}],
-#     'Pose': {'Pitch': -5.742334365844727,
-#      'Roll': -3.6629204750061035,
-#      'Yaw': 4.655891418457031},
-#     'Quality': {'Brightness': 56.744651794433594,
-#      'Sharpness': 99.95819854736328}},
-#    'Id': '1SK7cR8M',
-#    'MatchConfidence': 100.0,
-#    'Name': 'Jeff Bezos',
-#    'Urls': ['www.imdb.com/name/nm1757263']}],
-#  'OrientationCorrection': 'ROTATE_0',
-#  'ResponseMetadata': {'HTTPHeaders': {'connection': 'keep-alive',
-#    'content-length': '847',
-#    'content-type': 'application/x-amz-json-1.1',
-#    'date': 'Thu, 02 Aug 2018 22:17:38 GMT',
-#    'x-amzn-requestid': 'dd259a44-96a1-11e8-9943-9d73667d5b20'},
-#   'HTTPStatusCode': 200,
-#   'RequestId': 'dd259a44-96a1-11e8-9943-9d73667d5b20',
-#   'RetryAttempts': 0},
-#  'UnrecognizedFaces': []}
+#Funcion para un reconocimiento general de los elementos
+def recognize_object_and_scenes(bucketName, filePath):
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucketName,
+                'Name': filePath
+            }
+        }
+    )
+
+    return response
+
+#Funcion para saber si es necesario llamar a la api de celebridades
+def comprobar_si_hay_persona(labels):
+
+    names = ['Human', 'People', 'Person', 'Face']
+
+    for label in labels:
+        if(label['Name'] in names):
+            return True
+    
+    return False
+
+
+#Funcion para saber si la api trae resultados
+def parsear_resultado_object_and_scenes(response, label):
+
+    try:
+        if((label in response) and (len(response[label]) != 0)):
+            return response[label]
+        else:
+            return {}
+    except:
+        print('No hay labels')
+        return {}
+
