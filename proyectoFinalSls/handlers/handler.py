a0c89d09f02e96f8ebc3a6413db3a41d7c88c289
ALBERTO CRESPO SANCHEZ <67391243@iecisa.corp>
2018-08-04 15:45:53 +0200

    Insertar datos en dynamoDB

--- proyectoFinalSls/handlers/handler.py
+++ proyectoFinalSls/handlers/handler.py
@@ -8,7 +8,8 @@ def image_uploaded(event, context):
 
     file_obj = event['Records'][0]
     filePath = str(file_obj['s3']['object']['key'])
-    fileName = filePath.split('input_images/')[1]
+    folder = filePath.split('/')[0]
+    fileName = filePath.split('/')[1]
     bucketName = file_obj['s3']['bucket']['name']
     
 
@@ -16,22 +17,22 @@ def image_uploaded(event, context):
     response = utils_rekognition.recognize_object_and_scenes(bucketName, filePath)
 
     #Vemos si hay resultados
-    labels_object = utils_rekognition.parsear_resultado_object_and_scenes(response, 'Labels')
+    labels_object = utils.parsear_resultado_object_and_scenes(response, 'Labels')
 
     #Comprobamos si hay labels y si existen personas en ellas
     size = len(labels_object) != 0
-    personas = utils_rekognition.comprobar_si_hay_persona(labels_object)
+    personas = utils.comprobar_si_hay_persona(labels_object)
     labels_celebrities = {}
     if(size and personas):
         #LLamamos a la api de celebridades
         response = utils_rekognition.recognize_celebrities(bucketName, filePath)
-        labels_celebrities = utils_rekognition.parsear_resultado_object_and_scenes(response, 'CelebrityFaces')
+        labels_celebrities = utils.parsear_resultado_object_and_scenes(response, 'CelebrityFaces')
     
     print(labels_object)
     print(labels_celebrities)
     
     #Guardamos los resultados en dynamo
-    #metodo para hacerlo
+    utils_dynamodb.create_item(labels_object, labels_celebrities, folder, fileName)
     
 
 
--- proyectoFinalSls/handlers/utils_dynamodb.py
+++ proyectoFinalSls/handlers/utils_dynamodb.py
@@ -3,39 +3,32 @@ import time
 import uuid
 
 import boto3
+from boto3.dynamodb.types import DYNAMODB_CONTEXT
+import decimal
+from handlers import utils
+
+# Inhibit Inexact Exceptions
+DYNAMODB_CONTEXT.traps[decimal.Inexact] = 0
+# Inhibit Rounded Exceptions
+DYNAMODB_CONTEXT.traps[decimal.Rounded] = 0
+
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-def getCelebrities():
-    return table.scan()['Items']
-  
-def read(id):
-    res = table.get_item(
-        Key={
-            'id': id
-        }
-    )
 
-    return res['Item'] if 'Item' in res else None
 
-def create(image_uuid, celebrity):
-    timestamp = str(int(time.time() * 1000))
+
+#Funcion para crear la entidad en dynamoDB
+def create_item(labels_object, labels_celebrities, periodico, idimagen):
+    
     item = {
-        'id': str(uuid.uuid4()),
-        'idImage': image_uuid,
-        'idCelebrity': celebrity['Id'],
-        'name': celebrity['Name'],
-        'url': celebrity['Urls'][0] if celebrity['Urls'] else '-',
-        #Si no viene url, deberia poner None, pero dynamo pone true, ¿que ponemos si no trae url? un guion? vacio?
-        'createdAt': timestamp
-    }
-
+        'daymonthYear': utils.obtener_fecha_actual(),
+        'periodico': periodico,
+        'idimagen': idimagen,
+        'Labels': utils.parsear_datos_object_escenes(labels_object),
+        'CelebrityFaces': utils.parsear_datos_celebreties(labels_celebrities)
+        }
+    
     table.put_item(Item=item)
 
-# EJEMPLO ITEM A GUARDAR
-# {'createdAt': '1533338638369',
-#  'id': '3ed1eba8-4106-4da0-98fd-97f2374f08fa',
-#  'idCelebrity': '2So4RX0w',
-#  'idImage': '4c1bd5ea-9774-11e8-8dbf-0242ac110002',
-#  'name': 'Justin Trudeau',
-#  'url': 'www.imdb.com/name/nm0874040'}
+
